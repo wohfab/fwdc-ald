627963d86128291e205dac49787b73fb4cb8aee6
wohfab <wohfab@gmail.com>
2021-02-05 17:11:37 +0100

    fix: reduce file sizes

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,5 @@
 const yaml = require("js-yaml");
 const { DateTime } = require("luxon");
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const htmlmin = require("html-minifier");
 const markdownIt = require("markdown-it");
 const Image = require("@11ty/eleventy-img");
@@ -54,9 +53,6 @@ module.exports = function (eleventyConfig) {
     () => `${new Date().getFullYear()}`
   );
 
-  // Syntax Highlighting for Code blocks
-  eleventyConfig.addPlugin(syntaxHighlight);
-
   // To Support .yaml Extension in _data
   // You may remove this if you can use JSON
   eleventyConfig.addDataExtension("yaml", (contents) =>
@@ -71,8 +67,6 @@ module.exports = function (eleventyConfig) {
     "./_tmp/static/css/style.css": "./static/css/style.css",
     "./src/admin/config.yml": "./admin/config.yml",
     "./node_modules/alpinejs/dist/alpine.js": "./static/js/alpine.js",
-    "./node_modules/prismjs/themes/prism-tomorrow.css":
-      "./static/css/prism-tomorrow.css",
   });
 
   // Copy Image Folder to /_site
